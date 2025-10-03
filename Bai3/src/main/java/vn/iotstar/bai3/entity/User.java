4c9894c4a7ac3eb4494958524a8ebf20eea61648
nguyenit24 <nguyenhuynh.463459@gmail.com>
2025-10-03 21:53:04 +0700

    bai3

---
+++ Bai3/src/main/java/vn/iotstar/bai3/entity/User.java
@@ -0,0 +1,34 @@
+package vn.iotstar.bai3.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Email
+    private String email;
+    @Column(length = 60,columnDefinition = "navar(60)")
+    private String name;
+    private String password;
+
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
